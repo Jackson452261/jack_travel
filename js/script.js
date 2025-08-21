22f30972b6677d3b2a54cfc04f8b9ba2616b0f71
Jackson452261 <a411758@gmail.com>
2025-08-21 14:37:16 +0800

    增加部落格文章

--- js/script.js
+++ js/script.js
@@ -146,56 +146,8 @@ $(function () {
     }
   });
 
-  // Blog Navigation
-  $('.blog__card').on('click', function(e) {
-    e.preventDefault();
-    
-    const blogId = $(this).data('blog-id');
-    let blogUrl = '';
-    
-    switch(blogId) {
-      case 1:
-        blogUrl = 'blog-post-1.html';
-        break;
-      case 2:
-        blogUrl = 'blog-post-2.html';
-        break;
-      case 3:
-        blogUrl = 'blog-post-3.html';
-        break;
-      default:
-        return;
-    }
-    
-    // Navigate to the blog post page
-    window.location.href = blogUrl;
-  });
-  
-  // Handle "Read More" link clicks within blog cards
-  $('.blog__link').on('click', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    
-    const blogCard = $(this).closest('.blog__card');
-    const blogId = blogCard.data('blog-id');
-    let blogUrl = '';
-    
-    switch(blogId) {
-      case 1:
-        blogUrl = 'blog-post-1.html';
-        break;
-      case 2:
-        blogUrl = 'blog-post-2.html';
-        break;
-      case 3:
-        blogUrl = 'blog-post-3.html';
-        break;
-      default:
-        return;
-    }
-    
-    window.location.href = blogUrl;
-  });
+  // Blog Navigation - Remove this section since we're using direct HTML links
+  // The blog cards now use direct HTML links which work better
 
   // Questions Spoller
   $(".questions__label").on("click", function () {
